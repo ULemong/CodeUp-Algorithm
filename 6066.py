29aa980eceacd99113e43e90d3d7d9f1dff8aafd
ULemong <maintain9912@gmail.com>
2021-06-28 22:37:21 +0900

    [기초-조건/선택실행구조] 정수 3개 입력받아 짝/홀 출력하기

---
+++ 6066.py
@@ -0,0 +1,16 @@
+n1, n2, n3 = input().split()
+a = int(n1)
+b = int(n2)
+c = int(n3)
+if a%2 == 0:
+    print("even")
+else:
+    print("odd")
+if b%2 == 0:
+    print("even")
+else:
+    print("odd")
+if c%2 == 0:
+    print("even")
+else:
+    print("odd")
